806f6d9a4dbb2b38796b9f7b6f6871654eb560f1
Luís Ferreira de Oliveira Júnior <luis.oliveira@uft.edu.br>
2019-02-22 15:19:59 -0300

    finalização interface trabalho 1

--- CompiladorAlpha/CompiladorAlpha/MainWindow.xaml.cs
+++ CompiladorAlpha/CompiladorAlpha/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CompiladorAlpha
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
